97c54b4e033bd0a3b2149a261ed57c79f900ade8
rafasevero <rafasevero2302@gmail.com>
2024-10-07 15:33:01 -0300

    add funcionalidades

--- inserirProd.php
+++ inserirProd.php
@@ -5,11 +5,12 @@ $data_visita = $_POST['data_visita'];
 $quantidade = $_POST['quantidade'];
 $preco = $_POST['preco'];
 $desconto = $_POST['desconto'];
+$tipo_ingresso = $_POST['tipo_ingresso'];
 
 $conexao = mysqli_connect
 ('localhost','root','','projetofinal');
-$sql = "insert into ticket (data_visita, quantidade, preco, desconto)
-values ($data_visita, $quantidade, '$preco', '$desconto')";
+$sql = "insert into ticket (data_visita, quantidade, preco, desconto,tipo_ingresso)
+values ($data_visita, $quantidade, '$preco', '$desconto','$tipo_ingresso')";
 $executar = mysqli_query($conexao, $sql);
 
 if($executar){
--- topo.php
+++ topo.php
@@ -1,37 +1,31 @@
 <html>
   <head>
-    <style>
-	  body{color:blue; background-color:yellow;}
-	  p{color:red; background-color:white;}
-	  h1{color:orange; background-color:black;}
-	  ul{list-style:none; text-align:center;}
-	  li{display:inline-block;}
-	  a,a:visited{color:black;font-size:20px;text-decoration:none;border:1px solid black;border-radius:5px;margin:5px;padding:5px;}
-	  a:hover{color:yellow;font-size:20px;background-color:black;}
-	</style>
+  <Link rel="stylesheet" href="home.css">
+
   </head>
   <body>
-    <ul>
-	  <li><a href="home.html">Home</a></li>
-	  <?php
-	  if(isset($_SESSION['adm'])){ 
-	    if($_SESSION['adm'] == 1){
-	  ?>
-	      <li><a href="cadastroProd.html">Cadastrar Produtos</a></li>
-	      <li><a href="listar.php">Gerenciar Produtos</a></li>
-	  <?php
-	    }
-		else{
-	  ?>
-	  <li><a href="ver_produtos.php">Comprar</a></li>
-	  <li><a href="carrinho.php">Carrinho</a></li>
-	  <?php
-	    }
-		echo "<li><a href='logout.php'>Sair</a></li>";
-	  }
-	  else{
-		echo "<li><a href='home.html'>Logar</a></li>";
-	  }
-	  ?>
-	  
-	</ul>
+  <div class="main">
+    <nav class="navbar">
+        <div class="icon">
+            <h2 class="logo">BC World</h2>
+        </div>
+        <div class="menu">
+            <ul>
+                <li><a href="home.html">HOME</a></li>
+
+                <?php if (isset($_SESSION['adm'])): ?>
+                    <?php if ($_SESSION['adm'] == 1): ?>
+                        <li><a href="cadastroProd.html">Cadastrar Ingresso</a></li>
+                        <li><a href="listar.php">Gerenciar Ingresso</a></li>
+                    <?php else: ?>
+                        <li><a href="ver_produtos.php">Comprar</a></li>
+                        <li><a href="carrinho.php">Carrinho</a></li>
+                    <?php endif; ?>
+                    <li><a href='logout.php'>Sair</a></li>
+                <?php else: ?>
+                    <li><a href='home.html'>Logar</a></li>
+                <?php endif; ?>
+            </ul>
+        </div>
+    </nav>
+</div>
